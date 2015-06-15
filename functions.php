aa225eb57442613764c6d4bd1f2ab78d80722a30
Karissa <k@karissademi.com>
2015-06-15 10:59:45 -0400

    Theme tweaks

--- functions.php
+++ functions.php
@@ -50,58 +50,6 @@ function filter_site_upload_size_limit( $size ) {
 }
 add_filter( 'upload_size_limit', 'filter_site_upload_size_limit', 20 );
 
-
-if (!class_exists('LandbankCustomPostTypes')){
-    class LandbankCustomPostTypes{
-        function create_person_profile() {
-          register_post_type( 'person_page',
-            array(
-                'labels' => array(
-                    'name' => __( 'Person Profile' ),
-                    'singular_name' => __( 'Person Profile' ),
-                    'add_new'   => __('Add Person Profile'),
-                    'all_items'   => __('All Person Profiles'),
-                    'add_new_item' => __('Add Person Profile'),
-                    'edit_item'   => __('Edit Person Profile'),
-                    'view_item'   => __('View Person Profile'),
-                    'search_items'   => __('Search Person Profiles'),
-                    'not_found'   => __('Person Profile Not Found'),
-                    'not_found_in_trash'   => __('Person Profile not found in trash'),
-              ),
-                'taxonomies' => array(''),
-                'public' => true,
-                'supports' => array(
-                  'title',
-                  'editor',
-                  'thumbnail'
-                  ),
-                'has_archive' => true,
-                'menu_position' => 5,
-                'menu_icon' => 'dashicons-businessman',
-                'hierarchical' => true,
-                'rewrite' => array(
-                    'slug' => 'profile',
-                ),
-            )
-          );
-        }
-
-    }//end class
-
-}
-
-
-if (class_exists("LandbankCustomPostTypes")){
-    $custom_post_types = new LandbankCustomPostTypes();
-}
-
-if (isset($custom_post_types)){
-    //actions
-    add_action( 'init', array($custom_post_types, 'create_person_profile'));
-
-    register_activation_hook( __FILE__, array($custom_post_types, 'rewrite_flush') );
-}
-
 /*-----------------------------------------------------------------------------------*/
 /*	Remove unnecessary post types
 /*-----------------------------------------------------------------------------------*/
@@ -110,6 +58,7 @@ function remove_medical_press_theme_features() {
    // remove Movie Custom Post Type
    remove_action( 'init', 'create_doctor_post_type' );
    remove_action( 'init', 'create_gallery_post_type' );
+	remove_action( 'init', 'create_service_post_type' );
 }
 
 add_action( 'after_setup_theme', 'remove_medical_press_theme_features', 10 );
--- home_template.php
+++ home_template.php
@@ -55,24 +55,6 @@ if (have_posts()):
         }
     endwhile;
 endif;
-?>
-<div class="home-search">
-    <div class="container">
-        <div class="row">
-          <div class="<?php bc_all('12'); ?>">
-          <div id="search" class="widget clearfix">
-              <form method="get" id="search-form" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
-                  <div>
-                      <input type="text" value="<?php the_search_query(); ?>" name="s" id="search-text" placeholder="<?php _e('Search', 'framework'); ?>"/>
-                      <input type="submit" id="search-submit" value=""/>
-                  </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-<?php
 
 
 /* Home Features */
